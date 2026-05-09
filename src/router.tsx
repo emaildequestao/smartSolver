47b0b1170eabef53018b82b9ab24b60037e0aae9
ViniciusFS1 <fioravante.vinicius@gmail.com>
2026-05-09 16:34:20 -0300

    Update router.tsx

--- src/router.tsx
+++ src/router.tsx
@@ -3,10 +3,12 @@ import Dashboard from './pages/Dashboard'
 import PageComplaint from './pages/PageComplaint'
 import PageGraphics from './pages/PageGraphics'
 import ImportantComplaints from './pages/ImportantComplaints'
+import Login from './pages/Login'
 
 export default function AppRoutes() {
   return (
     <Routes>
+      <Route path="/" element={<Login/>} />
       <Route path="/" element={<Dashboard/>} />
       <Route path="/complaint/:id" element={<PageComplaint/>} />
       <Route path="/graphics" element={<PageGraphics/>} />
